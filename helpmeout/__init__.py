d01281ef943cb79fcbfc66893d7a2ab174238e4b
DestinedCodes <yiradesat@gmail.com>
2023-09-30 20:08:47 +0100

    fix CORS error

--- helpmeout/__init__.py
+++ helpmeout/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
