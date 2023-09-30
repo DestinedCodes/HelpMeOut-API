74ac45d12a8965cbcbccae785f33d88b752d0abb
DestinedCodes <yiradesat@gmail.com>
2023-09-30 13:20:03 +0100

    add requirements

--- helpmeout/routes/recordings.py
+++ helpmeout/routes/recordings.py
@@ -1,13 +1,50 @@
-from flask import jsonify, request
+from flask import jsonify, request, send_file
+import nanoid
+import subprocess
+from io import BytesIO
+from datetime import datetime
 from helpmeout import app, db
 from helpmeout.models.recordings import Recordings
+from moviepy.editor import VideoFileClip, concatenate_videoclips, clips_array
 
-# Add a new screen record
+# Initialize a new screen recording
 @app.route('/api/recording', methods=['POST'])
-def add_screen_record():
-    data = request.get_json()
-    new_recording = Recordings(**data)
+def start_screen_record():
+    user_id = request.json.get('user_id')
+    if not user_id:
+        return jsonify({'error': 'user_id is required'}), 400
+    id = nanoid.generate(size=10)
+    title = id
+    time = datetime.now()
+
+    new_recording = Recordings(id=id, user_id=user_id, time=time, title=title)
     db.session.add(new_recording)
     db.session.commit()
-    return jsonify(data)
+
+    return jsonify({'id': id}), 201
+
+# An endpoint to add video in chunks to a recording
+@app.route('/api/recording/<id>', methods=['POST'])
+def add_video_chunk(id):
+    recording = Recordings.query.filter_by(id=id).first()
+    if not recording:
+        return jsonify({'error': 'recording not found'}), 404
+    video = request.files.get('video').read()
+    if not video:
+        return jsonify({'error': 'video is required'}), 400
+    if recording.video:
+        recording.video = recording.video + video
+    else:
+        recording.video = video
+    db.session.commit()
+    return jsonify({'message': 'video added'}), 201
+
+# An endpoint to get the video of a recording
+@app.route('/api/recording/<id>', methods=['GET'])
+def get_video(id):
+    recording = Recordings.query.filter_by(id=id).first()
+    if not recording:
+        return jsonify({'error': 'recording not found'}), 404
+    return send_file(BytesIO(recording.video), download_name=f'{recording.title}.webm', as_attachment=True)
+
 
