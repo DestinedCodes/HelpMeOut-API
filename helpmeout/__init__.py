0d704e3265d4368bc40e983cfd548af3077ed62f
DestinedCodes <yiradesat@gmail.com>
2023-10-02 14:31:04 +0100

    cors

--- helpmeout/__init__.py
+++ helpmeout/__init__.py
@@ -15,14 +15,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recordings.db'
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 db.init_app(app)
 
-with open('auth_swagger.yaml', 'r') as file:
-    swagger_config = yaml.load(file, Loader=yaml.FullLoader)
-
-#swagger_config = yaml.load(open('swagger.yaml'), Loader=yaml.FullLoader)
+swagger_config = yaml.load(open('swagger.yaml'), Loader=yaml.FullLoader)
 Swagger(app, template=swagger_config)
 
 
